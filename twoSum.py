788899a87ce739320046fbc5ddfc3cb4a1d2d338
Asif <asif.16f@gmail.com>
2025-12-15 18:04:45 +0000

    Optimize twoSum Solution

--- twoSum.py
+++ twoSum.py
@@ -1,9 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for idx, num in enumerate(nums):
-            for idx2, num2 in enumerate(nums):
-                if idx == idx2:
-                    continue
+        seen = {}
 
-                if num + num2 == target:
-                    return [idx, idx2]
+        for idx, num in enumerate(nums):
+            
+            if target - num in seen:
+                return [seen[target - num], idx]
+            
+            seen[num] = idx
