750c6a2b328612537b0e229564ef1a1684961fd4
Asif <asif.16f@gmail.com>
2025-12-15 17:29:47 +0000

    Complete solution

--- twoSum.py
+++ twoSum.py
@@ -1,3 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
+        for idx, num in enumerate(nums):
+            for idx2, num2 in enumerate(nums):
+                if idx == idx2:
+                    continue
+
+                if num + num2 == target:
+                    return [idx, idx2]
