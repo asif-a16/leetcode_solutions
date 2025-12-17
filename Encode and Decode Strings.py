938b6275940593314c2a5af8e083a5914a6e1b3b
Asif <asif.16f@gmail.com>
2025-12-17 21:59:37 +0000

    Implement Encode and Decode Strings

--- Encode and Decode Strings.py
+++ Encode and Decode Strings.py
@@ -1,5 +1,13 @@
 class Solution:
 
     def encode(self, strs: List[str]) -> str:
+        final_str = ""
+        for str in strs:
+            final_str += str
+            final_str += "🎄"
+        return final_str
 
     def decode(self, s: str) -> List[str]:
+        strs2 = s.split("🎄")
+        
+        return strs2[:-1]
