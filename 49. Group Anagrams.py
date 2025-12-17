4099a4abdd4707835dfcd19bba241bd4e4e092ed
Asif <asif.16f@gmail.com>
2025-12-17 19:07:58 +0000

    Optimize solution for 49.

--- 49. Group Anagrams.py
+++ 49. Group Anagrams.py
@@ -1,37 +1,9 @@
-def isAnagram(s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        
-        for char in set(s):
-            if s.count(char) != t.count(char):
-                return False
-            
-        return True
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = []
-        added = {}
+        from collections import defaultdict
+        anagram_group = defaultdict(list)
+        for word in strs:
+            key = "".join(sorted(word))
+            anagram_group[key].append(word)
 
-        for i in range(len(strs)):
-            selected_word = strs[i]
-
-            if i in added:
-                continue
-
-            temp = [selected_word]
-            added[i] = True
-
-            for j in range(i + 1, len(strs)):
-                current_word = strs[j]
-                
-                if j in added:
-                    continue
-
-                if isAnagram(selected_word, current_word):
-                    temp.append(current_word)
-                    added[j] = True
-            
-            res.append(temp)
-
-        return res
+        return list(anagram_group.values())
