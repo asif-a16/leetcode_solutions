54900234ca4db9203fb1ca6430bc43365edc47d0
Asif <asif.16f@gmail.com>
2025-12-17 21:20:53 +0000

    Create gitignore

--- tests.py
+++ tests.py
@@ -1,9 +1,2 @@
-from collections import defaultdict
-frequencies = defaultdict(int)
-nums = [1,2,2,3,3,3]
-for num in nums:
-    frequencies[num] += 1
-
-print(frequencies.items())
-sorted_freq = sorted(frequencies.items(), key=lambda item: item[1], reverse=True)
-print(sorted_freq[0])
+if __name__ == "__main__":
+    print()
