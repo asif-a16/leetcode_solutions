9d7fe1dfeef0c97a27a53b28a4a365d99961e934
Asif <asif.16f@gmail.com>
2025-12-18 15:29:38 +0000

    Implement solution to 36.

--- 36. Valid Sudoku.py
+++ 36. Valid Sudoku.py
@@ -1,3 +1,31 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        board_t = list(zip(*board))
         
+        def containsDuplicates(listt: List[str]) -> bool:
+            for i in range(1, 10):
+                if listt.count(str(i)) >= 2:
+                    return True
+            return False
+
+        for row in board:
+            if containsDuplicates(row):
+                return False
+            
+        for col in board_t:
+            if containsDuplicates(col):
+                return False
+            
+        for col_idx in range(0, 9, 3):
+            for row_idx in range(0, 9, 3):
+
+                working = [
+                    board[i][j] 
+                    for i in range(row_idx, row_idx + 3) 
+                    for j in range(col_idx, col_idx + 3)
+                    ]
+                
+                if containsDuplicates(working):
+                    return False
+                
+        return True
