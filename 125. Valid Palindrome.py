9cf51b56d6f10ba60e39d4052f20b88bf136da41
Asif <asif.16f@gmail.com>
2025-12-18 22:35:02 +0000

    Implement solution to 125.

--- 125. Valid Palindrome.py
+++ 125. Valid Palindrome.py
@@ -1,3 +1,14 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        
+        import re
+        import math
+        clean_text = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+        s_len = len(clean_text)
+        l = 0
+        r = s_len - 1
+
+        for i in range(math.ceil(s_len / 2)):
+            if clean_text[i] == clean_text[r - i]:
+                continue
+            return False
+        return True
