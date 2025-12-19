a0230f93a99ecf1760a5019771b1f19b4595dfe6
Asif <asif.16f@gmail.com>
2025-12-19 10:45:04 +0000

    Implement brute-force solution to 15.

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -1,3 +1,13 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
+        n_len = len(nums)
+        res = []
+        for i in range(n_len):
+            for j in range(i + 1, n_len):
+                for k in range(j+1, n_len):
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        sort_res = sorted([nums[i], nums[j], nums[k]])
+                        if not(sort_res in res):
+                            res.append(sort_res)
+
+        return res
