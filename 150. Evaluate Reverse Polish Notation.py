88e2a934e857be5fecbc0e7109f03baf8448a351
Asif <asif.16f@gmail.com>
2025-12-19 23:22:03 +0000

    Implement solution to 150.

--- 150. Evaluate Reverse Polish Notation.py
+++ 150. Evaluate Reverse Polish Notation.py
@@ -1,3 +1,25 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for char in tokens:
+            match char:
+                case "+":
+                    op2 = stack.pop()
+                    op1 = stack.pop()
+                    stack.append(op1 + op2)
+                case "-":
+                    op2 = stack.pop()
+                    op1 = stack.pop()
+                    stack.append(op1 - op2)
+                case "*":
+                    op2 = stack.pop()
+                    op1 = stack.pop()
+                    stack.append(op1 * op2)
+                case "/":
+                    op2 = stack.pop()
+                    op1 = stack.pop()
+                    stack.append(int(op1 / op2))
+                case _:
+                    stack.append(int(char))
         
+        return stack[0]
