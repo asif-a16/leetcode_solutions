a4d0eabf9561fa1853ca1299c375a1ddb0fd4f87
Asif <asif.16f@gmail.com>
2025-12-19 15:57:56 +0000

    Cleanup solution to 11.

--- 11. Container With Most Water.py
+++ 11. Container With Most Water.py
@@ -13,9 +13,7 @@ class Solution:
 
             if height[left] < height[right]:
                 left += 1
-            elif height[right] < height[left]:
-                right -= 1
             else:
-                left += 1
+                right -= 1
 
         return best
