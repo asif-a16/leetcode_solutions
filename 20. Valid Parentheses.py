d232b490f621aebca6118dffdff0d12f199e285b
Asif <asif.16f@gmail.com>
2025-12-19 22:18:32 +0000

    Implement solution to 20.

--- 20. Valid Parentheses.py
+++ 20. Valid Parentheses.py
@@ -1,4 +1,24 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        
+        open_bracket = {"(", "[", "{"}
+        for char in s:
+            if char in open_bracket:
+                stack.append(char)
+                continue
+            
+            if not stack:
+                return False
+            
+            match char:
+                case ")":
+                    if stack.pop() != "(":
+                        return False
+                case "]":
+                    if stack.pop() != "[":
+                        return False
+                case "}":
+                    if stack.pop() != "{":
+                        return False
+            
+        return not stack
