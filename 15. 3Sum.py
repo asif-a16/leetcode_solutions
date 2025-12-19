b51a5c161f0d761ea08406f6e62d2ae3564cde9c
Asif <asif.16f@gmail.com>
2025-12-19 11:11:40 +0000

    Improve solution to 15.

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -1,13 +1,18 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         n_len = len(nums)
-        res = []
-        for i in range(n_len):
-            for j in range(i + 1, n_len):
-                for k in range(j+1, n_len):
-                    if nums[i] + nums[j] + nums[k] == 0:
-                        sort_res = sorted([nums[i], nums[j], nums[k]])
-                        if not(sort_res in res):
-                            res.append(sort_res)
+        res = set()
+        sett = {}
 
-        return res
+        for i in range(n_len):
+            pivot = -nums[i]
+            for j in range(i+1, n_len):
+                if pivot - nums[j] in sett:
+                    k = sett[pivot - nums[j]]
+                    sorted_ans = tuple(sorted([nums[i], nums[k], nums[j]]))
+                    if i != k and k != j and i != j:
+                        res.add(tuple(sorted_ans))
+                    continue
+                sett[nums[j]] = j
+    
+        return list(res)
