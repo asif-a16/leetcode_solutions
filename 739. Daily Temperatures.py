a26ec2bb26eeea1477a40ad952074bbca5d027e8
Asif <asif.16f@gmail.com>
2025-12-20 13:20:43 +0000

    Implement solution to 739. Daily Temperatures

--- 739. Daily Temperatures.py
+++ 739. Daily Temperatures.py
@@ -1,3 +1,16 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        
+        n = len(temperatures)
+        answer = [0] * n
+        stack = []
+        for i in range(n):
+            while stack and len(stack) >= 1:
+                if stack[-1][0] < temperatures[i]:
+                    lower_idx = stack[-1][1]
+                    answer[lower_idx] = i-lower_idx
+                    stack.pop()
+                    continue
+                break
+            stack.append((temperatures[i], i))
+
+        return answer
