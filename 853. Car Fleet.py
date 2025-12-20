b430bda5e115ca9883bf5d2626a9bcfb515a1d7d
Asif <asif.16f@gmail.com>
2025-12-20 15:33:46 +0000

    Implement solution to 853.

--- 853. Car Fleet.py
+++ 853. Car Fleet.py
@@ -1,3 +1,18 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        
+        stack = []
+        vehicles = sorted([(position[i], speed[i]) for i in range(len(position))], reverse=True)
+        for mile, speed in vehicles:
+            if not stack:
+                stack.append((mile, speed))
+                continue
+            stack_mile, stack_speed = stack[-1]
+            delta_speed = (speed - stack_speed)
+            if delta_speed == 0:
+                stack.append((mile, speed))
+                continue
+            time = (stack_mile - mile) / delta_speed
+            if time < 0 or (time * stack_speed + stack_mile) > target:
+                stack.append((mile, speed))
+
+        return len(stack)
