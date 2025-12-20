784212277cde5435b78c2d7213f028104db8421e
Asif <asif.16f@gmail.com>
2025-12-20 13:38:42 +0000

    Implement optimized solution to 739.

--- 739. Daily Temperatures.py
+++ 739. Daily Temperatures.py
@@ -1,16 +1,11 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        n = len(temperatures)
-        answer = [0] * n
+        answer = [0] * len(temperatures)
         stack = []
-        for i in range(n):
-            while stack and len(stack) >= 1:
-                if stack[-1][0] < temperatures[i]:
-                    lower_idx = stack[-1][1]
-                    answer[lower_idx] = i-lower_idx
-                    stack.pop()
-                    continue
-                break
-            stack.append((temperatures[i], i))
+        for i, temp in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < temp:
+                stack_idx = stack.pop()
+                answer[stack_idx] = i-stack_idx
+            stack.append(i)
 
         return answer
