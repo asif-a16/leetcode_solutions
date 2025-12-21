aadd5bb0ac0d876bb8302ebb9c86c21114012347
Asif <asif.16f@gmail.com>
2025-12-21 21:12:27 +0000

    Implement optimized solution to 875.

--- 875. Koko Eating Bananas.py
+++ 875. Koko Eating Bananas.py
@@ -1,25 +1,19 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         import math
-        lower_k = math.ceil(sum(piles) / h)
-        upper_k = max(piles)
-        best_k = -1
-        best_distance_to_h = 10**18
 
-        while lower_k <= upper_k:
-            k = lower_k + int((upper_k - lower_k) / 2)
-            new_h = 0
-            for num in piles:
-                new_h += math.ceil(num / k)
-            
-            distance_to_h = abs(h - new_h)
-            if distance_to_h <= best_distance_to_h and new_h <= h:
-                best_k = k
-                best_distance_to_h = distance_to_h
+        left = math.ceil(sum(piles) / h)
+        right = max(piles)
 
-            if new_h > h:
-                lower_k = k + 1
+        while left < right:
+            k = (left + right) // 2
+            hours = 0
+            for p in piles:
+                hours += (p + k - 1) // k
+
+            if hours > h:
+                left = k + 1
             else:
-                upper_k = k - 1
+                right = k
 
-        return best_k
+        return left
