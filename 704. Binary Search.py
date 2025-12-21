caa79c680654553d1e56774f02024bc32657a4fe
Asif <asif.16f@gmail.com>
2025-12-21 17:18:10 +0000

    Implement solution to 704.

---
+++ 704. Binary Search.py
@@ -0,0 +1,14 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            pivot = left + (right - left) // 2
+            if target == nums[pivot]:
+                return pivot
+            elif target < nums[pivot]:
+                right = pivot - 1
+            else:
+                left = pivot + 1
+
+        return -1
