73877d93487abd846f3428b642f76258498bd33a
Asif <asif.16f@gmail.com>
2025-12-22 18:37:33 +0000

    Implement optimized solution to 981.

--- 981. Time Based Key-Value Store.py
+++ 981. Time Based Key-Value Store.py
@@ -1,32 +1,14 @@
-from collections import defaultdict
-import bisect
-
 class TimeMap:
-
     def __init__(self):
-        self.map = defaultdict(list)
+        self.times = defaultdict(list)
+        self.values = defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        bisect.insort(self.map[key], (timestamp, value))
-        
+        self.times[key].append(timestamp)
+        self.values[key].append(value)
+
     def get(self, key: str, timestamp: int) -> str:
-        bucket = self.map[key]
-        if not bucket:
+        i = bisect.bisect_right(self.times[key], timestamp) - 1
+        if i < 0:
             return ""
-        left = 0
-        right = len(bucket) - 1
-        while left <= right:
-            pivot = left + int((right - left) / 2)
-            if bucket[pivot][0] == timestamp:
-                return bucket[pivot][1]
-            elif timestamp < bucket[pivot][0]:
-                right = pivot - 1
-            else:
-                left = pivot + 1
-        min_idx = min(left, right)
-        return bucket[min_idx][1] if bucket[min_idx][0] <= timestamp else ""
-        
-# Your TimeMap object will be instantiated and called as such:
-# obj = TimeMap()
-# obj.set(key,value,timestamp)
-# param_2 = obj.get(key,timestamp)
+        return self.values[key][i]
