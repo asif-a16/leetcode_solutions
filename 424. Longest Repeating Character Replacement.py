bec8b0b6a5ba1c52ae1bb830064ee44650de43c7
Asif <asif.16f@gmail.com>
2025-12-25 13:51:05 +0000

    Implement optimized solution to 424.

--- 424. Longest Repeating Character Replacement.py
+++ 424. Longest Repeating Character Replacement.py
@@ -3,11 +3,13 @@ class Solution:
         count = {}
         left = 0
         result = 0
+        maxf = 0
 
         for right in range(len(s)):
             count[s[right]] = count.get(s[right], 0) + 1
+            maxf = max(maxf, count[s[right]])
             
-            if right - left + 1 - max(count.values()) > k:
+            if right - left + 1 - maxf > k:
                 count[s[left]] -= 1
                 left += 1
 
