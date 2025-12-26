99e10c0aeb38c40cc74f721a38387319becfe1bf
Asif <asif.16f@gmail.com>
2025-12-26 20:51:51 +0000

    Cleanup code for 21.

--- 21. Merge Two Sorted Lists.py
+++ 21. Merge Two Sorted Lists.py
@@ -7,11 +7,10 @@ class Solution:
             if list1.val < list2.val:
                 current.next = list1
                 list1 = list1.next
-                current = current.next
             else:
                 current.next = list2
                 list2 = list2.next
-                current = current.next
+            current = current.next
 
         if list1:
             current.next = list1
