0850b8fc8482d6750c39b6c3612a5463b285eede
Asif <asif.16f@gmail.com>
2025-12-26 21:35:17 +0000

    Cleanup solution to 143.

--- 143. Reorder List.py
+++ 143. Reorder List.py
@@ -1,14 +1,6 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 from collections import deque
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        """
-        Do not return anything, modify head in-place instead.
-        """
         nodes = deque()
 
         while head:
@@ -22,6 +14,5 @@ class Solution:
                 right = nodes.pop()
                 right.next = nodes[0] if nodes else None
                 continue
-            if nodes:
-                remaining = nodes.pop()
-                remaining.next = None
+
+            nodes.pop().next = None
