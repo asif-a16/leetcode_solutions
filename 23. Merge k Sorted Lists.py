0dcb6bba5c04b49ed3d0fc20b2102d9a7911bc96
Asif <asif.16f@gmail.com>
2025-12-28 23:38:21 +0000

    Implement heap solution to 23.

--- 23. Merge k Sorted Lists.py
+++ 23. Merge k Sorted Lists.py
@@ -3,33 +3,24 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+import heapq
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if not lists: return None
 
-        def mergeList(l1, l2):
-            dummy = ListNode()
-            tail = dummy
-            while l1 and l2:
-                if l1.val < l2.val:
-                    tail.next = ListNode(l1.val)
-                    l1 = l1.next
-                    tail = tail.next
-                else:
-                    tail.next = ListNode(l2.val)
-                    l2 = l2.next
-                    tail = tail.next
-            if l1:
-                tail.next = l1
-            if l2:
-                tail.next = l2
-            return dummy.next
+        heap = []
+        for i, node in enumerate(lists):
+            if node:
+                heapq.heappush(heap, (node.val, i, node))
 
-        while len(lists) > 1:
-            merged_lists = []
-            for i in range(0, len(lists), 2):
-                l1 = lists[i]
-                l2 = lists[i + 1] if i + 1 < len(lists) else None
-                merged_lists.append(mergeList(l1, l2))
-            lists = merged_lists
-        return lists[0]
+        dummy = ListNode(0)
+        curr = dummy
+
+        while heap:
+            _, i, node = heapq.heappop(heap)
+            curr.next = node
+            curr = curr.next
+            if node.next:
+                heapq.heappush(heap, (node.next.val, i, node.next))
+
+        return dummy.next
