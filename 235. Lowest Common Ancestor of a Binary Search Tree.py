e8057b102413d46e103aedfeef808615ba1e38c7
Asif <asif.16f@gmail.com>
2025-12-29 20:47:24 +0000

    Implement optimized solution to 235.

--- 235. Lowest Common Ancestor of a Binary Search Tree.py
+++ 235. Lowest Common Ancestor of a Binary Search Tree.py
@@ -7,45 +7,12 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        values = {p.val, q.val}
+        cur = root
 
-        def LCA(node):
-            if not node:
-                return -1
-            
-            left = LCA(node.left)
-            right = LCA(node.right)
-
-            if isinstance(left, TreeNode):
-                return left
-            if isinstance(right, TreeNode):
-                return right
-            
-            if left in values and right in values and left != right:
-                return node
-            
-            if left == p.val:
-                if node.val == q.val:
-                    return node
-                else:
-                    return left
-            if right == p.val:
-                if node.val == q.val:
-                    return node
-                else:
-                    return right
-                
-            if left == q.val:
-                if node.val == p.val:
-                    return node
-                else:
-                    return left
-            if right == q.val:
-                if node.val == p.val:
-                    return node
-                else:
-                    return right
-                
-            return node.val
-        
-        return LCA(root)
+        while cur:
+            if p.val > cur.val and q.val > cur.val:
+                cur = cur.right
+            elif p.val < cur.val and q.val < cur.val:
+                cur = cur.left
+            else:
+                return cur
