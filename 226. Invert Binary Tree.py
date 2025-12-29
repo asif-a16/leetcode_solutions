aeed25f202ba9d079abd6aac99303c9e97f9ec09
Asif <asif.16f@gmail.com>
2025-12-29 12:25:36 +0000

    Improve readability of solution to 226.

--- 226. Invert Binary Tree.py
+++ 226. Invert Binary Tree.py
@@ -11,7 +11,7 @@ class Solution:
         
         root.left, root.right = root.right, root.left
 
-        root.left = self.invertTree(root.left)
-        root.right = self.invertTree(root.right)
+        self.invertTree(root.left)
+        self.invertTree(root.right)
         
         return root
