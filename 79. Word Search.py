a23dbdf8ca69067c0f1ae792eb5d90ad54d39802
Asif <asif.16f@gmail.com>
2026-01-02 21:23:12 +0000

    Implement optimizations to 79.

--- 79. Word Search.py
+++ 79. Word Search.py
@@ -1,6 +1,17 @@
+from collections import Counter
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         self.result = False
+
+        word_counts = Counter(word)
+        board_counts = Counter(char for row in board for char in row)
+        
+        for char, count in word_counts.items():
+            if board_counts[char] < count:
+                return False
+            
+        if board_counts[word[0]] > board_counts[word[-1]]:
+            word = word[::-1]
         
         def search(row: int, col: int, word_idx: int, visited: set):
             if word_idx == len(word) - 1:
@@ -28,7 +39,7 @@ class Solution:
                  search(pos[0], pos[1], word_idx + 1, visited.copy())
 
         for row in range(len(board)):
-            for col in range(len(board[row])):
+            for col in range(len(board[0])):
                 if board[row][col] == word[0]:
                     search(row, col, word_idx=0, visited=set())
                 if self.result:
