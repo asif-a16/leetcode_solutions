5540deb4a5785908d94e5fd964b3ca9adb8eecef
Asif <asif.16f@gmail.com>
2026-01-08 14:23:28 +0000

    Update solution to 70.

--- 70. Climbing Stairs.py
+++ 70. Climbing Stairs.py
@@ -2,7 +2,7 @@ class Solution(object):
     def climbStairs(self, n):
         a, b = 0, 1
 
-        for _ in range(1, n + 1):
+        for _ in range(n):
             a, b = b, a + b
 
         return b
