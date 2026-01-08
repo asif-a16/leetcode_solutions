883995db0058f6c00190f852028d34d0c3dd68f2
Asif <asif.16f@gmail.com>
2026-01-08 19:29:52 +0000

    Update solution to 198.

--- 198. House Robber.py
+++ 198. House Robber.py
@@ -1,17 +1,8 @@
 class Solution(object):
     def rob(self, nums):
-        if len(nums) == 1:
-            return nums[0]
-        if len(nums) == 2:
-            return max(nums[0], nums[1])
-        
-        prev = max(nums[0], nums[1])
-        prev_prev = nums[0]
+        prev, prev_prev = 0, 0
 
-        for i in range(2, len(nums)):
-            if nums[i] + prev_prev > prev:
-                prev_prev, prev = prev, nums[i] + prev_prev
-            else:
-                prev_prev = prev
+        for num in nums:
+            prev_prev, prev = prev, max(num + prev_prev, prev)
         
         return prev
