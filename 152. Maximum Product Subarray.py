6e2818fcc6bb83b7bce6634118906b575f9309bb
Asif <asif.16f@gmail.com>
2026-01-11 23:18:31 +0000

    Implement optimized solution to 152.

--- 152. Maximum Product Subarray.py
+++ 152. Maximum Product Subarray.py
@@ -1,31 +1,13 @@
 class Solution:
-    def maxProdSearch(self, nums: List[int]) -> int:
-        max_found = nums[0]
-        previous_negative_subarray = None
-        current_subarray = 1
-
-        for num in nums:
-            if num == 0:
-                max_found = max(max_found, 0)
-                current_subarray = 1
-                previous_negative_subarray = None
-                continue
-
-            if num < 0:
-                if previous_negative_subarray:
-                    current_subarray *= previous_negative_subarray * num
-                    previous_negative_subarray = None
-                    max_found = max(max_found, current_subarray)
-                else:
-                    previous_negative_subarray = current_subarray * num
-                    current_subarray = 1
-                continue
-
-            current_subarray *= num
-            max_found = max(max_found, current_subarray)
-        
-        return max_found
-
     def maxProduct(self, nums: List[int]) -> int:
-        return max(self.maxProdSearch(nums), 
-                   self.maxProdSearch(nums[::-1]))
+        cur_max = cur_min = result = nums[0]
+
+        for num in nums[1:]:
+            if num < 0:
+                cur_max, cur_min = cur_min, cur_max
+
+            cur_max = max(num, cur_max * num)
+            cur_min = min(num, cur_min * num)
+            result = max(result, cur_max)
+
+        return result
