0caa3b621cbd4a6e8a7088c8adaa09f1bccedf88
Asif <asif.16f@gmail.com>
2026-01-13 15:27:07 +0000

    Update solution to 139.

--- 139. Word Break.py
+++ 139. Word Break.py
@@ -1,6 +1,6 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        cache = [False] * len(s) + 1
+        cache = [False] * (len(s) + 1)
         cache[len(s)] = True
 
         for i in range(len(s), -1, -1):
