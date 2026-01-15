7b8d3cf7d3401a97692397bc7982b00df715818a
Asif <asif.16f@gmail.com>
2026-01-15 20:45:33 +0000

    Implement alternative solution to 338.

--- 338. Counting Bits.py
+++ 338. Counting Bits.py
@@ -1,13 +1,8 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
         result = [0] * (n + 1)
-        current_highest_power_of_two = None
 
         for num in range(1, n + 1):
-            if not num & (num - 1):
-                current_highest_power_of_two = num
-                result[num] = 1
-            else:
-                result[num] = 1 + result[num - current_highest_power_of_two]
+            result[num] = 1 + result[num & (num - 1)]
 
         return result
