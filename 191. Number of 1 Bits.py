ff31b889acbb62b3e26aed022189863068e840dd
Asif <asif.16f@gmail.com>
2026-01-15 19:40:04 +0000

    Implement better solution to 191.

--- 191. Number of 1 Bits.py
+++ 191. Number of 1 Bits.py
@@ -1,11 +1,8 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        count = 0
         res = 0
-        while 1 << count <= n:
-            if n & (1 << count):
-                res += 1
-
-            count += 1
+        while n:
+            n &= (n-1)
+            res += 1
 
         return res
