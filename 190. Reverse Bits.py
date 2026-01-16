f6f5894ba1dbfc1f619e58d0e52b4f9ee5668e90
Asif <asif.16f@gmail.com>
2026-01-16 12:13:40 +0000

    Implement a more readable solution to 190.

--- 190. Reverse Bits.py
+++ 190. Reverse Bits.py
@@ -1,7 +1,9 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        result = 0
-        for left_shift_amount in range (32):
-            result = (result << 1) | bool(n & (1 << left_shift_amount))
+        res = 0
 
-        return result
+        for shift_amount in range(32):
+            bit = (n >> shift_amount) & 1
+            res |= bit << (31 - shift_amount)
+
+        return res
