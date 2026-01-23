127b9d38324baf2fddc5a8f6c45ebf7ea6f73f33
Asif <asif.16f@gmail.com>
2026-01-23 20:43:38 +0000

    Update solution to 417.

--- 417. Pacific Atlantic Water Flow.py
+++ 417. Pacific Atlantic Water Flow.py
@@ -1,28 +1,35 @@
-from collections import deque
+from typing import List
+
 
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         m, n = len(heights), len(heights[0])
-        
-        def bfs_from_ocean(start_cells):
-            queue = deque(start_cells)
-            visited = set(start_cells)
+        pacific = set()
+        atlantic = set()
+
+        def dfs(r, c, visit, prev_height):
+            if ((r, c) in visit or
+                r < 0 or c < 0 or r == m or c == n or
+                heights[r][c] < prev_height):
+                return
             
-            while queue:
-                r, c = queue.popleft()
-                for dr, dc in [(0,1),(1,0),(0,-1),(-1,0)]:
-                    nr, nc = r + dr, c + dc
-                    if (0 <= nr < m and 0 <= nc < n and 
-                        (nr, nc) not in visited and 
-                        heights[nr][nc] >= heights[r][c]):
-                        visited.add((nr, nc))
-                        queue.append((nr, nc))
-            return visited
-        
-        pacific_start = [(0, c) for c in range(n)] + [(r, 0) for r in range(1, m)]
-        pacific_reachable = bfs_from_ocean(pacific_start)
-         
-        atlantic_start = [(m-1, c) for c in range(n)] + [(r, n-1) for r in range(m-1)]
-        atlantic_reachable = bfs_from_ocean(atlantic_start)
-        
-        return list(pacific_reachable & atlantic_reachable)
+            visit.add((r, c))
+            possible_positions = [
+                (r + 1, c), # down
+                (r, c + 1), # right
+                (r - 1, c), # up
+                (r, c - 1)  # down
+            ]
+
+            for row, col in possible_positions:
+                dfs(row, col, visit, heights[r][c])
+
+        for c in range(n):
+            dfs(0, c, pacific, heights[0][c])
+            dfs(m - 1, c, atlantic, heights[m - 1][c])
+
+        for r in range(m):
+            dfs(r, 0, pacific, heights[r][0])
+            dfs(r, n - 1, atlantic, heights[r][n - 1])
+
+        return list(atlantic & pacific)
