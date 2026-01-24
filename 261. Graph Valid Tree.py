8ede2965feb7942ff9de141e28f26efe94bf3223
Asif <asif.16f@gmail.com>
2026-01-24 18:08:45 +0000

    Implement alternative solution to 261.

--- 261. Graph Valid Tree.py
+++ 261. Graph Valid Tree.py
@@ -1,39 +1,26 @@
-class ValHolder:
-    def __init__(self, value: int):
-        self.value = value
-
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if not edges:
+        if not n:
             return True
         
-        graph = {}
-        counter = 0
-        graph_ids = []
+        adj = { i:[] for i in range(n)}
 
         for node1, node2 in edges:
-            if node1 == node2:
-                return False
-            if node1 not in graph and node2 not in graph:
-                new_val = ValHolder(counter)
-                graph[node1] = new_val
-                graph[node2] = new_val
-                graph_ids.append(new_val)
-                counter += 1
-            elif node1 in graph and node2 not in graph:
-                graph[node2] = graph[node1]
-            elif node1 not in graph and node2 in graph:
-                graph[node1] = graph[node2]
-            elif node1 in graph and node2 in graph and graph[node1].value != graph[node2].value:
-                min_val = min(graph[node2].value, graph[node1].value)
-                for id in graph_ids:
-                    if id.value == graph[node2].value or id.value == graph[node1].value:
-                        continue
-                    id.value -= 1
-                graph[node2].value = min_val
-                graph[node1].value = min_val
-                counter -= 1
-            else:
-                return False
+            adj[node1].append(node2)
+            adj[node2].append(node1)
 
-        return counter == 1
+        visited = set()
+        def dfs(node, prev_node):
+            if node in visited:
+                return False
+            
+            visited.add(node)
+            for neighbour_node in adj[node]:
+                if neighbour_node == prev_node:
+                    continue
+                if not dfs(neighbour_node, node):
+                    return False
+                
+            return True
+        
+        return dfs(0, -1) and len(visited) == n
