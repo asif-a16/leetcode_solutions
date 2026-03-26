cf0883b58b95fe2ab910a34f72b49db1e979a8e7
Asif <asif.16f@gmail.com>
2026-03-26 23:26:24 +0000

    Implement solution to 1143.

--- 1143. Longest Common Subsequence.py
+++ 1143. Longest Common Subsequence.py
@@ -1,25 +1,11 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        longer, shorter = text1, text2
-
-        if len(text2) > len(text1):
-            longer, shorter = text2, text1
-
-        if longer == shorter:
-            return len(longer)
-        
-        lengths = [0] * len(longer)
-        
-        short_pointer = 0
-
-        for i, letter in enumerate(longer):
-            if letter == shorter[0]:
-                lengths[i] = 1
-                short_pointer = 1
-            elif letter == shorter[short_pointer]:
-                lengths[i] = lengths[i-1] + 1
-                short_pointer += 1
-                if short_pointer == len(shorter):
-                    return len(shorter)
-                
-        return max(lengths)
+        def recurse(text1_idx: int, text2_idx: int) -> int:
+            if text1_idx >= len(text1) or text2_idx >= len(text2):
+                return 0
+            if text1[text1_idx] == text2[text2_idx]:
+                return 1 + recurse(text1_idx + 1, text2_idx + 1)
+            else:
+                return max(recurse(text1_idx + 1, text2_idx), recurse(text1_idx, text2_idx + 1))
+            
+        return recurse(0, 0)
