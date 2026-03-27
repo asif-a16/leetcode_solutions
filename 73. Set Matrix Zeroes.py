a7a170081abebb5ed7f81d9427faa2360d18a660
Asif <asif.16f@gmail.com>
2026-03-27 18:36:39 +0000

    Implement solution to 73.

--- 73. Set Matrix Zeroes.py
+++ 73. Set Matrix Zeroes.py
@@ -3,4 +3,16 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        
+        zeroed_rows = [0] * len(matrix)
+        zeroed_cols = [0] * len(matrix[0])
+
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                if matrix[row][col] == 0:
+                    zeroed_rows[row] = 1
+                    zeroed_cols[col] = 1
+
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                if zeroed_rows[row] or zeroed_cols[col]:
+                    matrix[row][col] = 0
