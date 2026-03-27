9cc241a7a42abe3fb41cec0874691799b5f32096
Asif <asif.16f@gmail.com>
2026-03-27 17:00:08 +0000

    Implement solution to 48.

--- 48. Rotate Image.py
+++ 48. Rotate Image.py
@@ -3,4 +3,10 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        
+        #transpose 
+        for row in range(len(matrix)):
+            for col in range(row, len(matrix)):
+                matrix[row][col], matrix[col][row] = matrix[col][row], matrix[row][col]
+
+        for row in matrix:
+            row.reverse()
