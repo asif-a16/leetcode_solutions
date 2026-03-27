da943baa63af84dae77c324060aa089199c303cc
Asif <asif.16f@gmail.com>
2026-03-27 22:25:48 +0000

    Implement solution to 211.

--- 211. Design Add and Search Words Data Structure.py
+++ 211. Design Add and Search Words Data Structure.py
@@ -1,13 +1,38 @@
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.is_terminal = False
+
 class WordDictionary:
 
     def __init__(self):
-        
+        self.root = Node()
 
     def addWord(self, word: str) -> None:
+        cur = self.root
+        for char in word:
+            if char not in cur.children:
+                cur.children[char] = Node()
+            cur = cur.children[char]
+
+        cur.is_terminal = True
         
 
-    def search(self, word: str) -> bool:
+    def search(self, word: str, root: Node = None) -> bool:
+        cur = self.root
+
+        if root != None:
+            cur = root
+
+        for i, char in enumerate(word):
+            if char == ".":
+                return any([self.search(word[i + 1:], cur.children[child]) for child in cur.children.keys()])
+            elif char not in cur.children:
+                return False
+            else:
+                cur = cur.children[char]
         
+        return cur.is_terminal
 
 
 # Your WordDictionary object will be instantiated and called as such:
