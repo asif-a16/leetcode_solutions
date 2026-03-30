eb422334e9187c8368f7ec35a1a9832ee55988bf
Asif <asif.16f@gmail.com>
2026-03-30 23:28:42 +0100

    Implement solution to 57.

--- 57. Insert Interval.py
+++ 57. Insert Interval.py
@@ -1,3 +1,28 @@
+import bisect
+
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        bisect.insort(intervals, newInterval)
+        if not intervals:
+            return intervals
         
+        result = []
+        cur_interval = intervals[0]
+
+        def overlapsWith(interval1: list[int], interval2: list[int]) -> bool:
+            if ((interval1[1] >= interval2[0] and interval2[0] >= interval1[0]) or 
+                (interval1[0] <= interval1[1] and interval1[1] >= interval2[0])):
+                return [min(interval1[0], interval2[0]), max(interval1[1], interval2[1])]
+            return False
+
+        for interval in intervals:
+            merged_interval = overlapsWith(cur_interval, interval)
+
+            if not merged_interval:
+                result.append(cur_interval)
+                cur_interval = interval
+            else:
+                cur_interval = merged_interval
+
+        result.append(cur_interval)
+        return result
